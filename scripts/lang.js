01227d83fd529f90e94e5d8be549350f523fec61
Denis Kazakov <madarauchiha6969@yandex.ru>
2022-11-25 20:25:58 +0300

    Добавил смену языка

---
+++ scripts/lang.js
@@ -0,0 +1,26 @@
+const langArr = {
+  unit: {
+    ru: "Путешествия по России",
+    en: "Travel in Russia",
+  },
+  lead__title: {
+    ru: "Путешествия по России тест на язык",
+    en: "Travel in Russia",
+  },
+  memory: {
+    ru: "объем памяти",
+    en: "memory size",
+  },
+  "memory-type": {
+    ru: "тип памяти",
+    en: "memory type",
+  },
+  cool: {
+    ru: "тип системы охлаждения",
+    en: "сooling system type",
+  },
+  more: {
+    ru: "детально о товаре",
+    en: "more details",
+  },
+};
